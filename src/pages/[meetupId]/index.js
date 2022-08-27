e30ce8aefc04468fcfe6ad66aeaa7015018d4df0
Leigh West <leighwest@hotmail.com>
2022-08-27 02:32:12 +0000

    Added meetup details page and styling

---
+++ src/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/b/b0/Melbourne_Skyline_and_Princes_Bridge_-_Dec_2008_%28cropped%29.jpg"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
