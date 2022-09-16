19e3d7edcbb64281e61673b53856c05a4dcda813
Leigh West <leighwest@hotmail.com>
2022-09-16 09:17:33 +0000

    Initial mongoDb setup

---
+++ src/pages/api/new-meetup.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://leighwest:vO9P0xrdN8YN@cluster0.tnkff0l.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
